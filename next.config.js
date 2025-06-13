054e34dfaefa1d29c4842b33b56c78fd5fc68d4b
MartinSurgeon <agbenyenusemartin@gmail.com>
2025-06-13 06:21:13 +0000

    Fix: Temporarily disable PWA to troubleshoot deployment styling issues

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: true // Temporarily set to true to disable PWA for all environments
 });
 
 /** @type {import('next').NextConfig} */
